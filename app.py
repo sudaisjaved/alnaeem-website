55d64f2780c626d461a33e43d35a0861ac2b13d6
sudaisjaved <97953935+sudaisjaved@users.noreply.github.com>
2024-01-08 17:50:14 +0000

    added an API route, but there is some more work to be done

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [
+{
+  'id' : 1,
+  'title' : 'Neckless',
+  'location' : 'Dubai, Gold Souk',
+  'Price' : 'DHs. 4,500'
+},
+  {
+    'id' : 2,
+    'title' : 'Chain',
+    'location' : 'Dubai, Gold Souk',
+    'Price' : 'DHs. 2,000'
+  },
+
+{
+  'id' : 3,
+  'title' : 'Ring',
+  'location' : 'Dubai, Gold Souk',
+  'Price' : 'DHs. 650'
+},
+{
+  'id' : 4,
+  'title' : 'Nosering',
+  'location' : 'Dubai, Gold Souk',
+  'Price' : 'DHs. 110'
+}
+]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Al Naeem')
+
+@app.route("jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
